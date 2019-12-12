39afc690008a290a7dfcff4d97b15c4d48c5156c
Asjad Syed <syedasjadsyed@gmail.com>
2019-12-12 03:16:05 -0500

    Implemented rest of the platform which includes: Allowing the user to toggle whether their clothes are weather proof or not (on the front-end and back-end) Giving the user suggested outfits based on items uploaded in Inventory and current weather Allowing the user to get a different outfit Informing the user on current local weather
    Co-authored-by: hkidwai
    Co-authored-by: Saqibism

--- assistant.js
+++ assistant.js
@@ -1,3 +1,8 @@
+import * as inventory from './inventory.js';
+import { replaceItem } from './inventory.js';
+
+//export let itemTypes = ['hats', 'shirts', 'pants', 'accessories'];
+
 export const getLatitudeLongitude = async (jwt) => {
     let result = await axios({
         method: "get",
@@ -43,18 +48,119 @@ export const getForecastHourlyData = async (forecastHourlyEndpoint) => {
     return result.data;
 };
 
-$(async () => {
+export const getIconType = (icon) => {
+    let re = new RegExp('.*/(.+)[,?]');
+    let iconType = icon.match(re)[1];
+    return iconType;
+}
+
+export const needWeatherProofClothing = (iconType) => {
+    let needWeatherProofClothingMap = {
+        "skc": false,
+        "few": false,
+        "sct": false,
+        "bkn": false,
+        "ovc": false,
+        "wind_skc": false,
+        "wind_few": false,
+        "wind_sct": false,
+        "wind_bkn": false,
+        "wind_ovc": false,
+        "snow": true,
+        "rain_snow": true,
+        "rain_sleet": true,
+        "snow_sleet": true,
+        "fzra": true,
+        "rain_fzra": true,
+        "snow_fzra": true,
+        "sleet": true,
+        "rain": true,
+        "rain_showers": true,
+        "rain_showers_hi": true,
+        "tsra": true,
+        "tsra_sct": true,
+        "tsra_hi": true,
+        "tornado": true,
+        "hurricane": true,
+        "tropical_storm": true,
+        "dust": true,
+        "smoke": true,
+        "haze": true,
+        "hot": true,
+        "cold": true,
+        "blizzard": true,
+        "fog": false,
+    }
+    return !!needWeatherProofClothingMap[iconType];
+}
+
+export const getNeedWeatherProofClothing = async () => {
 
     let jwt = localStorage.getItem('jwt')
     let { latitude, longitude } = await getLatitudeLongitude(jwt)
-    console.log(latitude, longitude)
     let weatherData = await getWeatherData(latitude, longitude);
+    let forecastEndpoint = weatherData.properties.forecast;
     let forecastHourlyEndpoint = weatherData.properties.forecastHourly;
-    console.log(forecastHourlyEndpoint);
+    let forecastData = await getForecastHourlyData(forecastEndpoint)
     let forecastHourlyData = await getForecastHourlyData(forecastHourlyEndpoint)
-    console.log(forecastHourlyData);
-    let soonestPeriod = forecastHourlyData.properties.periods[0];
-    console.log(soonestPeriod)
-    let shortForecast = soonestPeriod.shortForecast;
-    console.log(shortForecast)
+    let soonestPeriod = forecastData.properties.periods[0];
+    let soonestHourlyPeriod = forecastHourlyData.properties.periods[0];
+    let shortForecast = soonestHourlyPeriod.shortForecast;
+    let detailedForecast = soonestPeriod.detailedForecast;
+
+    //detailedForecast
+    $('#todays-weather-detailed').html(detailedForecast)
+    
+    let icon = soonestHourlyPeriod.icon;
+    
+    let iconType = getIconType(icon);
+    //alert(iconType)
+    
+    let willNeedWeatherProofClothing = needWeatherProofClothing(iconType);
+    //alert(willNeedWeatherProofClothing)
+    return willNeedWeatherProofClothing;
+}
+
+let pick = (arr) => {
+    let choice = arr[Math.floor(Math.random() * arr.length)];
+    return choice;
+}
+
+let suggest = async () => {
+    let willNeedWeatherProofClothing = await getNeedWeatherProofClothing();
+    ///willNeedWeatherProofClothing = false;
+    for (let itemType of inventory.itemTypes) {
+        let itemRoot = $(`#${itemType}-item-root`)
+        let items = await inventory.getAllItems(itemType);
+        let filtered = items.filter(item => {
+            let clothingIsWeatherProof = Object.values(item)[0]['weatherProof'];
+            return (willNeedWeatherProofClothing == clothingIsWeatherProof)
+        })
+        
+        ///console.log(`filtered for ${willNeedWeatherProofClothing}`, filtered);
+
+        let choice = pick(filtered);
+        // let temp = Object.values(choice)[0]['weatherProof'];
+        // console.log(temp)
+        if (choice === undefined) { // sometimes pick is will be given [], so it will return undefined. if so we pass
+            ///console.log(itemType, 'did filtered')
+            continue;
+        } else {
+            ///console.log(itemType, 'not filtered')
+            //console.log('filtered',choice)
+        }
+
+        //console.log(choice)
+        ///console.log(Object.keys(choice)[0])
+        let choiceDataURL = Object.keys(choice)[0]
+        inventory.replaceItem(choiceDataURL, itemType)
+    }
+}
+
+$(async () => {
+    
+    await suggest();
+    let resuggestSuggestionButton = $(`#resuggest-suggestion-button`);
+    resuggestSuggestionButton.on('click', (event) => {window.location.reload()});
+
 });
--- inventory.js
+++ inventory.js
@@ -1,13 +1,124 @@
-let itemTypes = ['hats', 'shirts', 'pants', 'accessories'];
+export let itemTypes = ['hats', 'shirts', 'pants', 'accessories'];
 
-let prependToColumn = (item, itemType) => {
+let prependToColumn = (dataURL, itemType) => {
     let column = $(`#${itemType}-column-root`);
-    let renderedItem = renderItem(item);
+    let renderedItem = renderItem(dataURL);
     column.prepend(renderedItem);
 }
 
+export let replaceItem = (dataURL, itemType) => {
+    let item = $(`#${itemType}-item-root`);
+    //debugger;
+    let renderedSimpleItem = renderSimpleItem(dataURL);
+    //alert(renderedItem)
+    item.html(renderedSimpleItem);
+    //$('body').append(renderedItem)
+}
+
+let toggleWeatherProof = async (dataURL, itemType, wasWeatherProof) => {
+
+    let nowWeatherProof = !wasWeatherProof;
+    let jwt = localStorage.getItem('jwt');
+
+    // const result = await axios({
+    //     method: 'delete',
+    //     url: `http://localhost:3000/user/wardrobe/${itemType}/${encodeURI(dataURL)}`,
+    //     headers: {
+    //         Authorization: `Bearer ${jwt}`
+    //     },
+    //     /*data: {
+    //         data: {
+    //             [dataURL]: {
+    //                 weatherProof: nowWeatherProof
+    //             }
+    //         },
+    //         type: 'merge'
+    //     },*/
+    // })
+
+    const result2 = await axios({
+        method: 'post',
+        url: `http://localhost:3000/user/wardrobe/${itemType}/`,
+        headers: {
+            Authorization: `Bearer ${jwt}`
+        },
+        data: {
+            data: {
+                [dataURL]: {
+                    weatherProof: nowWeatherProof
+                }
+            },
+            type: 'merge'
+        },
+    }).then((response) => {
+
+    }).catch((error) => {
+
+    });
+
+    /*let result = await axios({
+        method: "post",
+        url: 'http://localhost:3000/user/geolocation/',
+        headers: {
+            Authorization: `Bearer ${jwt}`
+        },
+        data: {
+            data: {
+                latitude: latitude,
+                longitude: longitude,
+            }
+        }
+    }).then(async response => {
+
+    }).catch(async error => {
+
+    });*/
+}
+
+function itemButtonClick(event) {
+    //console.log(event.currentTarget)
+    let itemType = $(event.currentTarget).closest('.column-root').get(0).id.slice(0, -12) // slice off '-column-root'
+    let wasWeatherProof = $(event.currentTarget).hasClass('is-link')
+    if (wasWeatherProof) {
+        $(event.currentTarget).removeClass('is-link').addClass('is-warning')
+    } else {
+        $(event.currentTarget).removeClass('is-warning').addClass('is-link')
+    }
+    //alert(wasWeatherProof);
+    let img = $(event.currentTarget).siblings()[0];
+    let src = img.getAttribute('src');
+
+    toggleWeatherProof(src, itemType, wasWeatherProof);
+};
+
+let set = new Set();
+
+export const renderSimpleItem = (dataURL) => {
+    let html = $.parseHTML(`<div class="notification has-text-centered item-div"><img class="item-image" src="${dataURL}" style="border-radius: 5%;">
+    </div>`);
+    return html[0];
+}
 export const renderItem = (dataURL) => {
-    return $.parseHTML(`<div class="notification"><img src="${dataURL}" style="border-radius: 5%;"></div>`)[0]
+    //$('body').on('click','.itemButton', editButtonClick);
+    //<textarea class="textarea" placeholder="Enter description"></textarea>
+    // 
+    //$(html).find('.itemButton').on('click', editButtonClick)
+    //console.log(html[0])
+    if (set.has(dataURL)) {
+        return $.parseHTML(``)
+    } else {
+        set.add(dataURL);
+    }
+
+
+    let html = $.parseHTML(`<div class="notification has-text-centered item-div"><img class="item-image" src="${dataURL}" style="border-radius: 5%;">
+    <button class="button item-button is-warning">
+        <i class="fas fa-cloud-sun-rain fa-lg fa-fw"></i>
+    </button>
+    </div>`);
+    $(html).find('.item-button').on('click', itemButtonClick)
+    //debugger;
+    return html[0];
 };
 
 export const handleItemUpload = async function (event, itemType) {
@@ -24,11 +135,20 @@ export const handleItemUpload = async function (event, itemType) {
             let dataURL = event.target.result;
             //console.log(dataURL.slice(0, 75))
 
+            let jwt = localStorage.getItem('jwt');
             const result = await axios({
                 method: 'post',
-                url: `http://localhost:3000/public/wardrobe/${itemType}/`,
+                url: `http://localhost:3000/user/wardrobe/${itemType}/`,
+                headers: {
+                    Authorization: `Bearer ${jwt}`
+                },
                 data: {
-                    data: [dataURL],
+                    //data: [dataURL],
+                    data: {
+                        [dataURL]: {
+                            weatherProof: false
+                        }
+                    },
                     type: 'merge'
                 },
             }).then((response) => {
@@ -45,11 +165,16 @@ export const handleItemUpload = async function (event, itemType) {
 }
 
 export const getAllItems = async (itemType) => {
-    let itemURL = `http://localhost:3000/public/wardrobe/${itemType}`;
+    let itemURL = `http://localhost:3000/user/wardrobe/${itemType}`;
+    let jwt = localStorage.getItem('jwt');
     let result = axios({
         method: "get",
         url: itemURL,
+        headers: {
+            Authorization: `Bearer ${jwt}`
+        },
     }).then(response => {
+        //console.log(response.data.result)
         return response.data.result;
     }).catch(error => {
         return [];
@@ -65,8 +190,14 @@ $(async function () {
 
     for (let itemType of itemTypes) {
         let items = await getAllItems(itemType);
-        for (let item of items) {
-            prependToColumn(item, itemType);
+        let dataURLs = []
+        for (let x = 0; x < items.length; x++) {
+            //console.log(items[x])
+            dataURLs.push(Object.keys(items[x])[0])
+        }
+
+        for (let dataURL of dataURLs) {
+            prependToColumn(dataURL, itemType);
         }
     }
 });
